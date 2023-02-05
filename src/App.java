1a0f1ce9fac58164b78b8e51ed967a33e9a2ff80
abecode <abe.kazemzadeh@gmail.com>
2023-02-04 20:53:24 -0600

    added some string testing

---
+++ src/App.java
@@ -0,0 +1,27 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("Hello World xx ");
+        System.console( );
+        for (String key : System.getenv().keySet()) {
+            System.out.println("key:" + key + " value:" + System.getenv(key));
+        }
+        /*
+        System.out.println("multi
+        line");
+         */
+        final String s=null;
+        System.out.println("multiline\n" + "你好👋");
+        System.out.println(2+2 + "hi");
+        System.out.println("hi" + 2+2);
+        System.out.println('h');
+        //System.out.println('串')；
+        if (s != null){
+            System.out.println("hi "+ s);
+        }
+        char c = '串';
+        System.out.println(c);
+        System.out.println((int)3.5);
+    }
+}
